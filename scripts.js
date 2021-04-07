bdc1cfcdfa0bacd52101bffdf40396aa757e77ba
Laura Tissi <laura.tracierra@gmail.com>
2021-04-06 21:42:00 -0300

    js recebe o número de cartas e coloca as cartas na tela

--- scripts.js
+++ scripts.js
@@ -1,6 +1,12 @@
-    let numeroDeCartas = parseInt(prompt ("Com quantas cartas você quer jogar?"));
-while(numeroDeCartas%2 != 0 || numeroDeCartas > 14 || numeroDeCartas < 4){
-    alert("escolhe outro");
+let numeroDeCartas = parseInt(prompt ("Com quantas cartas você quer jogar?"));
+
+    while(numeroDeCartas%2 != 0 || numeroDeCartas > 14 || numeroDeCartas < 4){
     numeroDeCartas = parseInt(prompt ("Com quantas cartas você quer jogar?"));
-   
-}
+    }
+
+    for (let i = 1; i < numeroDeCartas; i++){
+        const elemento = document.querySelector(".container-carta");
+        elemento.innerHTML = elemento.innerHTML + `<div class="carta">
+        <img src="imagens/front.png" alt="papagaio inical">  
+      </div>`;
+    }
